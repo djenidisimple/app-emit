68c888aaa97801b3e5c4f18fb4919c1c83000349
djenidisimple <djenidigauss@gmail.com>
2026-05-25 23:13:58 +0300

    cleanup(backend): suppression des fichiers Admin non compilables et doublons (controllers, interfaces, validators, dto, services)

--- backend/AppEmit.API/Interfaces/Admin/IAdminStatsService.cs
+++
@@ -1,9 +0,0 @@
-// Interfaces/Admin/IAdminStatsService.cs
-using AppEmit.API.DTOs.Admin;
-
-namespace AppEmit.API.Interfaces.Admin;
-
-public interface IAdminStatsService
-{
-    Task<StatAdminDto> GetStatistiquesAsync();
-}
